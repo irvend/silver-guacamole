add7586e0e6763d08401e31fe9e876158ecdfe15
irvend <119556039+irvend@users.noreply.github.com>
2022-12-04 07:07:48 +0800

    Add files via upload

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html land="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>DIT LMS Landing Page</title>
+		<link rel="stylesheet" href="styles.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+	</head>
+	
+	<body>
+		<section>
+			<input type="checkbox" id="check">
+			<header>
+				<h2> <a href="#" class="logo">Logo</a> </h2>
+				<div class="navigation">
+					<a href="index.php" class="logo">Home</a>
+					<a href="#" class="logo">Instructors</a>
+					<a href="#" class="logo">Courses</a>
+					<a href="about.php" class="logo">About</a>
+					<a href="#" class="logo">Login/Sign Up</a>
+				</div>
+				<label for="check">
+					<i class="fas fa-bars menu-btn"></i>
+					<i class="fas fa-times close-btn"></i>
+				</label>
+			</header>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php include 'header.php';?>
+
+			<div class="content">
+				<div class="info">
+					<h2>Welcome to <br><span>DIT-LMS</span></h2>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+					<a href="#" class="info-btn">More Info</a>
+				</div>
+			</div>
+
+			<div class="media-icons">
+				<a href="#"><i class="fa-brands fa-facebook"></i></a>
+				<a href="#"><i class="fa-brands fa-twitter"></i></a>
+				<a href="#"><i class="fa-brands fa-instagram"></i></a>
+			</div>
+		</section>	
+	</body>	
+
+</html>	
